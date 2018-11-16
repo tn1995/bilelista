7445b198d513d973db3d0f46e6cebc1320aeaae8
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-11-16 13:08:59 +0200

    poistettu vahingossa väärään paikkaan luodut new.html ja list.html

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import BooleanField, StringField
 
 class TaskForm(FlaskForm):
     name = StringField("Task name")
- 
+    done = BooleanField("Done")
+  
     class Meta:
         csrf = False
--- application/tasks/views.py
+++ application/tasks/views.py
@@ -1,6 +1,8 @@
+from flask import render_template, request, redirect, url_for
+
 from application import app, db
-from flask import redirect, render_template, request, url_for
 from application.tasks.models import Task
+from application.tasks.forms import TaskForm
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():
@@ -8,7 +10,7 @@ def tasks_index():
 
 @app.route("/tasks/new/")
 def tasks_form():
-    return render_template("tasks/new.html")
+    return render_template("tasks/new.html", form = TaskForm())
   
 @app.route("/tasks/<task_id>/", methods=["POST"])
 def tasks_set_done(task_id):
@@ -21,8 +23,11 @@ def tasks_set_done(task_id):
 
 @app.route("/tasks/", methods=["POST"])
 def tasks_create():
-    t = Task(request.form.get("name"))
+    form = TaskForm(request.form)
 
+    t = Task(form.name.data)
+    t.done = form.done.data
+  
     db.session().add(t)
     db.session().commit()
   
