313d03bc90a2c8136983e11c1e8326b7ea966a9f
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-11-23 10:58:45 +0200

    lisätty roolit

--- application/auth/models.py
+++ application/auth/models.py
@@ -31,6 +31,9 @@ class User(Base):
 
     def is_authenticated(self):
         return True
+    
+    def roles(self):
+        return ["ADMIN"]
 
     @staticmethod
     def find_users_with_no_tasks():
