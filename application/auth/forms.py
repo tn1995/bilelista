e3a54a902f2261ddec2b27a1f78b62586fa26585
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-07 16:28:50 +0200

    lisättiin validointeja käyttäjänimen ja salasanan tekemiseen

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,7 +1,5 @@
-
-
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Username")
@@ -11,9 +9,9 @@ class LoginForm(FlaskForm):
         csrf = False
 
 class SignupForm(FlaskForm):
-    name = StringField("name")
-    username = StringField("Username")
-    password = PasswordField("Password")
+    name = StringField("name", [validators.Length(min=2, max=10)])
+    username = StringField("Username", [validators.Length(min=2, max=10)])
+    password = PasswordField("Password",  [validators.Length(min=2, max=10)])
   
     class Meta:
         csrf = False
