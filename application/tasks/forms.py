243430ff404b77b2ca9f1da913153f2d1754618d
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-07 08:41:49 +0200

    laitettu maksimi nimisyötteen pituudelle

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, validators
 
 class TaskForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=2)])
+    name = StringField("Task name", [validators.Length(min=2, max=30)])
     done = BooleanField("Done")
   
     class Meta:
