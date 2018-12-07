636229f2bbec1879d54edfd35a380ede70b3a8cf
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-07 08:43:29 +0200

    lisätty kommentit, mitä metodit tekevät

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -5,18 +5,21 @@ from application import app, db, login_required
 from application.tasks.models import Task
 from application.tasks.forms import TaskForm
 
+from application.auth.models import User
+from application.auth.forms import LoginForm, SignupForm
+
 
 @app.route("/tasks/", methods=["GET"])
 def tasks_index():
     return render_template("tasks/list.html", tasks = Task.query.all())
 
-  
+#creates new   
 @app.route("/tasks/new/")
 @login_required()
 def tasks_form():
     return render_template("tasks/new.html", form = TaskForm())
 
-  
+#sets tasks done  
 @app.route("/tasks/<task_id>/", methods=["POST"])
 @login_required()
 def tasks_set_done(task_id):
@@ -27,6 +30,7 @@ def tasks_set_done(task_id):
 
   
     return redirect(url_for("tasks_index"))
+#deletes tasks
 @app.route("/tasks/delete/<task_id>/", methods=["POST"])
 @login_required()
 def tasks_delete(task_id):
@@ -40,7 +44,7 @@ def tasks_delete(task_id):
   
     return redirect(url_for("tasks_index"))
 
-  
+#creates tasks  
 @app.route("/tasks/", methods=["POST"])
 @login_required()
 def tasks_create():
@@ -55,5 +59,16 @@ def tasks_create():
   
     db.session().add(t)
     db.session().commit()
+  
+    return redirect(url_for("tasks_index"))
+
+@app.route("/tasks/<task_id>/", methods=["POST"])
+@login_required()
+def tasks_participate(task_id):
+    a = User.get_id
+    t = Task.query.get(task_id)
+    b = UserTask(f)
+    db.session().commit()
+
   
     return redirect(url_for("tasks_index"))
