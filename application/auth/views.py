af7934d90db5653c5fb19c508cf50bb57a78ab6f
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-09 19:44:40 +0200

    lisättiin käyttäjätunnuksen validointeja, kuten pituus

--- application/auth/views.py
+++ application/auth/views.py
@@ -14,6 +14,7 @@ def auth_login():
     # mahdolliset validoinnit
 
     user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+    
     if not user:
         return render_template("auth/loginform.html", form = form,
                                 error = "No such username or password")
@@ -21,7 +22,7 @@ def auth_login():
 
     login_user(user)
     return redirect(url_for("index"))   
-
+#
 @app.route("/auth/signup", methods = ["GET", "POST"])
 def auth_signup():
     if request.method == "GET":
@@ -31,10 +32,21 @@ def auth_signup():
     # mahdolliset validoinnit
     
     user = User.query.filter_by(username=form.username.data).first()
+
+    if len(form.username.data) < 3 | len(form.username.data) > 20:
+        return render_template("auth/signupform.html", form = form,
+                                error = "Käyttäjätunnuksen tulee olla vähintää 3 merkkiä ja enintään 20 merkkiä")
+
     if user:
         return render_template("auth/signupform.html", form = form,
                                 error = "Käyttäjä on jo olemassa")
+    if form.password.data != form.repeatpassword.data:
+        return render_template("auth/signupform.html", form = form,
+                                error = "Salasanat eivät täsmää")
 
+    if len(form.password.data) < 3 | len(form.password.data) > 20:
+        return render_template("auth/signupform.html", form = form,
+                                error = "Salasanan tulee olla vähintää 3 merkkiä ja enintään 20 merkkiä")
     account = User(form.name.data)
     account.username = form.username.data
     account.password = form.password.data
