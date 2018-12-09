6fd66899670a98e6d16dcafd8bbdc559d041e918
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-09 23:12:02 +0200

    poistettiin turhaa kommentoitua koodia

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -7,30 +7,26 @@ class Task(Base):
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
     onupdate=db.func.current_timestamp())
-
     name = db.Column(db.String(144), nullable=False)
     done = db.Column(db.Boolean, nullable=False)
+    date = db.Column(db.String, nullable=False)
     #location_id = db.Column(db.Integer, db.ForeignKey('location.id'),
     #                       nullable=False)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
 
-    def __init__(self, name):
+    def __init__(self, name, done, date):
         self.name = name
         self.done = False
+        self.date = date
 
-
-    #Show all parties
+    #Show all partys
     @staticmethod
     def find_lista():
-        stmt = text("SELECT Account.name AS account_name, Task.id, Task.done, Task.name FROM Account"
+        stmt = text("SELECT Account.name AS account_name, Task.id, Task.done, Task.name, Task.date FROM Account"
                      " LEFT JOIN Task ON Task.account_id = Account.id"
                      " WHERE (Task.account_id = Account.id)"
                      " GROUP BY Task.id, account.name")
         res = db.engine.execute(stmt)
-
-#        response = []
-#        for row in res:
-#            response.append({"name":row[1]})
 
         return res
