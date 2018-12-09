125649c272ac06c7afa95fd149bc21075d5c7a0f
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-09 23:11:21 +0200

    Osa sivustolla näkyvästä tekstistä oli vielä englanniksi ja vaihdettiin se suomeksi

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, validators
+from wtforms import BooleanField, StringField, validators, DateField
 
 class TaskForm(FlaskForm):
-    name = StringField("Task name", [validators.Length(min=2, max=30)])
-    done = BooleanField("Done")
+    name = StringField("Bileiden nimi", [validators.Length(min=2, max=30)])
+    done = BooleanField("Ovatko bileet jo alkaneet")
+    date = DateField("Päivämäärä", format='%d.%m.%Y')
   
     class Meta:
         csrf = False
