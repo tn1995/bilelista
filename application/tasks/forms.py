a836f6f221eeb56523c8dad87ae6c98fa09f6b11
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-10 00:25:29 +0200

    ulkomuodon hiomista

--- application/tasks/forms.py
+++ application/tasks/forms.py
@@ -4,8 +4,8 @@ from wtforms import BooleanField, StringField, validators, DateField
 class TaskForm(FlaskForm):
     name = StringField("Bileiden nimi", [validators.Length(min=2, max=30)])
     done = BooleanField("Ovatko bileet jo alkaneet")
-    date = DateField("Päivämäärä", format='%d.%m.%Y')
-    klo = StringField("Kellonaika")
+    date = DateField("Pvm", format='%d.%m.%Y')
+    klo = StringField("Klo")
   
     class Meta:
         csrf = False
