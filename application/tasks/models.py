026a8814cc6908f3e32a198a8ef9edd9624931f9
Tuomas <tuomas.nurmiainen@helsinki.fi>
2018-12-14 13:39:21 +0200

    herokua varten muokattu tietokantaa, joka ei toiminut

--- application/tasks/models.py
+++ application/tasks/models.py
@@ -38,7 +38,7 @@ class Task(Base):
         stmt = text("SELECT Task.id, Task.name, Task.date FROM Account"
                      " LEFT JOIN Task ON Account.id = Task.account_id"
                      " WHERE (Account.username = :username)"
-                     " GROUP BY Account.id, task.name").params(username=username)
+                     " GROUP BY Account.id, task.name, task.id").params(username=username)
         res = db.engine.execute(stmt)
 
         response = []
